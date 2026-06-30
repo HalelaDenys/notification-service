cf6e51ae65b3bb75171012a9dc69379f3d6a8283
_Avvake_ <denisergo18@gmail.com>
2026-06-30 09:50:08 +0300

    feat(dlq): dlq worker
    -- add dlq worker
    -- update schemas
    -- update smtp worker

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -1,8 +1,10 @@
 __all__ = [
     "settings",
     "RetryPolicy",
+    "get_error_cause",
 ]
 
 
 from core.config import settings
 from core.retry_policy import RetryPolicy
+from core.utils import get_error_cause
